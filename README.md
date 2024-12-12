e4243b45571fa7a31b6be8aaa7c399d71d351bf3
czmatejt9 <vronvron@seznam.cz>
2024-12-12 13:06:47 +0100

    remove unused endpoint

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AK KURIM SERVER
 Api for mobile app and webpage (part of maturita)
+Mobile app - https://github.com/czmatejt9/AKKurim
--- app/api/v1/admin.py
+++ app/api/v1/admin.py
@@ -18,22 +18,3 @@ router = APIRouter(
     dependencies=[Depends(verify_session())],
 )
 SessionType = Annotated[SessionContainer, Depends(verify_session())]
-
-
-# only a one time setup to create the admin role TODO change it to add any role
-@router.get(
-    "/create-role",
-    status_code=204,
-    responses={201: {"description": "Created"}},
-)
-async def create_role(
-    session: SessionType,
-):
-    try:
-        await create_new_role_or_add_permissions("admin", ["read", "write"])
-        await add_role_to_user(
-            "public", "d81fbff7-ea4e-4fc4-8df3-f9b06619f0ea", "admin"
-        )
-        return Response(status_code=status.HTTP_201_CREATED)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
