8adfc2b1c7524364bb39b93c3bf55f63473f70cf
czmatejt <tajovsky.matej@gmail.com>
2024-12-31 22:34:25 +0100

    f

--- app/features/guardian/exceptions.py
+++ app/features/guardian/exceptions.py
@@ -2,4 +2,9 @@ from app.core.exceptions import CustomHTTPException
 
 
 class GuardianNotFoundException(CustomHTTPException):
-    super().__init__(status_code=404, detail="Guardian not found")
+    def __init__(
+        self,
+        status_code: int = 404,
+        detail: str = "Guardian not found",
+    ) -> None:
+        super().__init__(status_code=status_code, detail=detail)
