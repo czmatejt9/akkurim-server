428a2f4056243d3701e866f0e7dd0cd3c6bb8856
czmatejt <tajovsky.matej@gmail.com>
2025-01-03 21:22:12 +0100

    fix import err

--- app/core/remote_config/service.py
+++ app/core/remote_config/service.py
@@ -1,21 +1,17 @@
 from asyncpg import Connection
 
-from app.core.remote_config.exceptions import RemoteConfigNotFoundError
-from app.core.utils.sql_utils import convert_uuid_to_str, generate_sql_read
+from app.core.remote_config.schemas import RemoteConfigRead
+from app.core.utils.default_service import DefaultService
 
 
 class RemoteConfigService:
     def __init__(self) -> None:
-        self.table = "remote_config"
+        self.table: str = "remote_config"
+        self.default_service: DefaultService = DefaultService(
+            "remote_config",
+            "/remote_config/{id}",
+            RemoteConfigRead,
+        )
 
     async def get_remote_config(self, tenant_id: str, db: Connection) -> dict:
-        query, values = generate_sql_read(
-            tenant_id,
-            self.table,
-            ["welcome_message", "minimum_app_version"],
-        )
-        remote_config = await db.fetchrow(query, *values)
-        if not remote_config:
-            raise RemoteConfigNotFoundError(tenant_id)
-
-        return convert_uuid_to_str(dict(remote_config))
+        return await self.default_service.get_object_by_id(tenant_id, db)
