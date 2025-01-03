f3b81318f029690e6ca8501ca760116270baedb6
czmatejt <tajovsky.matej@gmail.com>
2025-01-03 16:24:39 +0100

    .

--- app/alembic/versions/7099f92a0135_add_club_table_and_profile_pic_for_.py
+++ app/alembic/versions/7099f92a0135_add_club_table_and_profile_pic_for_.py
@@ -23,22 +23,7 @@ def upgrade() -> None:
     conn.execute(
         sa.text(
             """
-        BEGIN;
-        CREATE TABLE club (
-            id text NOT NULL,
-            name text NOT NULL,
-            description text NOT NULL,
-            created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            PRIMARY KEY (id)
-        );
         
-        ALTER TABLE athlete
-            ADD profile_picture text;
-        ALTER TABLE athlete
-            ADD club_id text REFERENCES club(id) MATCH SIMPLE;
-            
-        COMMIT;
         """
         )
     )
