c873cbb054517906a991ba29275032ecb2b888d7
czmatejt <tajovsky.matej@gmail.com>
2025-01-12 21:58:57 +0100

    kk

--- app/initial_data.py
+++ app/initial_data.py
@@ -1,17 +1,20 @@
 from asyncio import get_event_loop
 
-from app.core.database import get_db
+from app.core.database import db, get_db
 
 
 async def create_athlete_status(name: str):
-    db = get_db()
+    await db.connect()
+    db_ = get_db()
     try:
-        await db.execute(
+        await db_.execute(
             "INSERT INTO athlete_status (id, name) VALUES (5f0e92e2-d123-11ef-9cd2-0242ac120002, $1)",
             name,
         )
     except Exception as e:
         print(e)
+    finally:
+        await db.disconnect()
 
 
 if __name__ == "__main__":
