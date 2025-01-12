170cc6d2525319edf5180414346c1801b352fe85
czmatejt <tajovsky.matej@gmail.com>
2025-01-12 21:41:38 +0100

    add await

--- app/initial_data.py
+++ app/initial_data.py
@@ -4,7 +4,7 @@ from app.core.database import get_db
 async def create_athlete_status(name: str):
     db = await get_db()
     try:
-        db.execute("INSERT INTO athlete_status (id, name) VALUES (1, $1)", name)
+        await db.execute("INSERT INTO athlete_status (id, name) VALUES (1, $1)", name)
     except Exception as e:
         print(e)
 
